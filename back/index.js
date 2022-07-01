18d259fd890a0ab0b3a3ff12a781cd4206cfabd4
jslee0510 <fw1eleven1@gmail.com>
2022-07-01 18:54:38 +0900

    백엔드 추가

--- back/index.js
+++ back/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const cors = require('cors')
-// const mysql = require('mysql2')
+const mysql = require('mysql2')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const schedule = require('node-schedule')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const dbconfig = {
+  host: 'localhost',
+  user: 'root',
+  database: 'electric',
+  password: '3456',
+}
+
+const connection = mysql.createConnection(dbconfig)
 
 const app = express()
 
@@ -21,11 +33,28 @@ app.get('/', (req, res) => {
   res.send('OK')
 })
 
+app.get('/exchange', (req, res) => {
+  try {
+    connection.query(
+      'SELECT * FROM exchange ORDER BY createtime DESC LIMIT 1',
+      function (err, result, field) {
+        if (err) {
+          return res.status(400).send(err)
+        }
+
+        res.json(result[0])
+      }
+    )
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
 app.get('/test', async (req, res) => {
   const url = req.query.url
   const response = await axios.get(
     // 'https://www.digimart.net/cat01/shop1484/DS07695772/'
-    'https://www.digimart.net/cat01/shop5356/DS07695782/'
+    url
   )
   const $ = cheerio.load(response.data)
   const priceElement = $('.itemDetailBlock .itemState .price')
@@ -37,6 +66,7 @@ app.get('/test', async (req, res) => {
 
     const regex = /\d+/
     price = regex.exec(price)
+
     const returnObj = {
       price: price[0],
       img: img,
@@ -46,6 +76,38 @@ app.get('/test', async (req, res) => {
   } catch {
     return res.status(400).send('error')
   }
+})
+
+const job = schedule.scheduleJob('10 11 * * 1-5', function () {
+  const result = axios.get(
+    'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON',
+    {
+      params: {
+        authkey: process.env.EXCHANGE_API_KEY,
+        data: 'AP01',
+      },
+    }
+  )
+
+  result
+    .then((response) => {
+      response.map((v, i) => {
+        if (v.cur_unit === 'JPY(100)') {
+          connection.query(
+            'INSERT INTO exchange (type, rate) VALUES (?, ?)',
+            [v.cur_unit, v.tts],
+            function (err, result, fields) {
+              if (err) {
+                console.log(err)
+              }
+            }
+          )
+        }
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 app.listen(app.get('port'), () => {
--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -1,16 +1,54 @@
-import axios from 'axios'
-import React, { FunctionComponent, useRef, useState } from 'react'
-import { atom, useRecoilState } from 'recoil'
-import { urlState } from '../states'
+import axios, { AxiosResponse } from 'axios'
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
+import styled from '@emotion/styled'
+
+interface ExchangeType {
+  rate: number
+  createtime: string
+}
+
+const initExchange = {
+  rate: 0,
+  createtime: '',
+}
+
+const AddComma = (num: number): string => {
+  const regexp = /\B(?=(\d{3})+(?!\d))/g
+  let comma = num.toString().replace(regexp, ',')
+  if (comma.indexOf('.') !== -1) {
+    return comma.slice(0, comma.indexOf('.'))
+  }
+
+  return comma
+}
 
 const Url = () => {
   const [img, setImg] = useState<string>('')
-  const [price, setPrice] = useState<number>(0)
+  const [exchange, setExchange] = useState<ExchangeType>(initExchange)
+  const [origPrice, setOrigPrice] = useState<number>(0)
+  const [checkConsume, setCheckConsume] = useState<boolean>(false)
+  const [totalPrice, setTotalPrice] = useState<number>(0)
+  const [addPrice, setAddPrice] = useState<number>(0)
   const [urlError, setUrlError] = useState<boolean>(false)
 
   const inputRef = useRef<HTMLInputElement>(null)
 
   // const [url, setUrl] = useRecoilState(urlState)
+
+  // get exchange rate
+  useEffect(() => {
+    const result = axios.get<ExchangeType>('http://localhost:5000/exchange')
+    result
+      .then((response) => {
+        setExchange({
+          rate: response.data.rate,
+          createtime: response.data.createtime,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
   const onClickButton = () => {
     if (inputRef.current) {
@@ -26,18 +64,29 @@ const Url = () => {
           console.log(response)
           if (response.status === 200) {
             setImg(response.data.img)
-            setPrice(response.data.price)
+            setOrigPrice(response.data.price)
 
             setUrlError(false)
           } else {
             setUrlError(true)
+            setOrigPrice(0)
           }
         })
         .catch((err) => {
           console.log(err)
           setUrlError(true)
+          setOrigPrice(0)
         })
     }
+  }
+
+  const onChangeConsume = () => {
+    if (checkConsume === true) {
+      setTotalPrice(origPrice * 0.9)
+    } else {
+      setTotalPrice(origPrice)
+    }
+    setCheckConsume(!checkConsume)
   }
 
   return (
@@ -62,71 +111,103 @@ const Url = () => {
         {urlError && <div>ERROR</div>}
       </div>
 
-      {price !== 0 && <div>url</div>}
+      {origPrice !== 0 && <div>url</div>}
       <div className="mt-10">
         <div className="flex">
-          <div className="w-1/2">image</div>
+          <div className="w-1/2 p-5">{/* {img && <img src={img} />} */}</div>
           <div className="w-1/2 flex flex-col pr-5">
             <div className="mb-10">
+              <label className="block w-full mb-2 ml-1">판매가격</label>
+              <span className="block w-full text-right text-sm pr-2">
+                ¥ {AddComma(origPrice)}
+              </span>
+              <div className="relative">
+                <div
+                  className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
+                  id="price"
+                >
+                  {AddComma((origPrice * exchange.rate) / 100)} 원
+                </div>
+              </div>
+            </div>
+
+            <hr className="py-2.5" />
+
+            <div className="mb-10">
+              <input type="checkbox" id="l1" onChange={onChangeConsume} />
+              <label className="mb-2 ml-3" htmlFor="l1">
+                직배송 받기 (소비세 제외)
+              </label>
+            </div>
+
+            {checkConsume && (
+              <div className="mb-10">
+                <label className="block w-full mb-2 ml-1">
+                  소비세 제외가격
+                </label>
+                <span className="block w-full text-right text-sm pr-2">
+                  ¥ {AddComma(origPrice * 0.9)}
+                </span>
+                <div className="relative">
+                  <div
+                    className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
+                    id="price"
+                  >
+                    {AddComma((origPrice * 0.9 * exchange.rate) / 100)} 원
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className="mb-10">
               <label className="block w-full mb-2 ml-1">Price</label>
               <span className="block w-full text-right text-sm pr-2">
-                10,000,000
+                ¥ 10,000,000
               </span>
-              <input
-                type="text"
-                className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
-                id="price"
-                readOnly
-                value="11,000,000"
-              />
+              <div className="relative">
+                <div
+                  className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
+                  id="price"
+                >
+                  11,000,000 원
+                </div>
+              </div>
             </div>
 
             <div className="mb-10">
               <label className="block w-full mb-2 ml-1">Price</label>
               <span className="block w-full text-right text-sm pr-2">
-                10,000,000
+                ¥ 10,000,000
               </span>
-              <input
-                type="text"
-                className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
-                id="price"
-                readOnly
-                value="11,000,000"
-              />
-            </div>
-
-            <div className="mb-10">
-              <label className="block w-full mb-2 ml-1">Price</label>
-              <span className="block w-full text-right text-sm pr-2">
-                10,000,000
-              </span>
-              <input
-                type="text"
-                className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
-                id="price"
-                readOnly
-                value="11,000,000"
-              />
+              <div className="relative">
+                <div
+                  className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
+                  id="price"
+                >
+                  11,000,000 원
+                </div>
+              </div>
             </div>
 
             <div className="mb-16">
               <label className="block w-full mb-2 ml-1">Price</label>
               <span className="block w-full text-right text-sm pr-2">
-                10,000,000
+                ¥ 10,000,000
               </span>
-              <input
-                type="text"
-                className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
-                id="price"
-                readOnly
-                value="11,000,000"
-              />
+              <div className="relative">
+                <div
+                  className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none w-full read-only:bg-gray-100 text-right cursor-default"
+                  id="price"
+                >
+                  11,000,000 원
+                </div>
+              </div>
             </div>
 
             <div>
               <div className="text-right text-sm">
-                <div>exchange</div>
-                <div>1,000</div>
+                <div>환율 정보 ({exchange.createtime.slice(0, 10)})</div>
+                <div>1000원 : {exchange.rate}엔</div>
               </div>
             </div>
           </div>
